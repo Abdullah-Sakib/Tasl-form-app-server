40f5d9b75abaf99ca52eb12be2170de1655a869f
Abdullah-Sakib <abdullahsakib888@gmail.com>
2022-12-18 17:38:55 +0600

    Connected the database

--- index.js
+++ index.js
@@ -2,9 +2,35 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
 
-app.use(cors());
+app.use(cors());  
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.3booq2e.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    const selectorsCollection = client.db("TaskForm").collection("selectors");
+
+
+    app.get('/selector', async(req, res) => {
+      const doc = {
+        selector: "Manufacturing",
+      };
+      const result = await selectorsCollection.insertOne(doc);
+      res.send(result)
+    })
+
+  }
+  finally{
+
+  }
+}
+run().catch(err => console.log(err))
 
 app.get('/', (req, res) => {
   res.send("Wow! Our server is running");
